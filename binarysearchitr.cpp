7f66cc450b6c3b02189e9c74e5c80cebd8d0de02
Ayush Kumar Srivastava <ayush050419.com>
2024-02-17 00:50:52 +0530

    first commit

---
+++ binarysearchitr.cpp
@@ -0,0 +1,16 @@
+//Binary searh using the iterative approach
+#include<bits/stdc++.h>
+using namespace std;
+int binarysearch(vector<int> a,int x){
+    for(int i=0;i<a.size();i++){
+        if(a[i]==x){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    vector<int> a={1,2,3,5,8,9,6,3,2,55,44};
+    int x=2;
+    cout<<binarysearch(a,x);
+}
